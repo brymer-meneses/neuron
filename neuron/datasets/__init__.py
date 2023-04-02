dff9718da4bd0c26a924531ef1f41923abe787a0
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-04-03 02:36:49 +0800

    fix internals of batch iterator

--- neuron/datasets/__init__.py
+++ neuron/datasets/__init__.py
@@ -1,2 +1,3 @@
 
 from neuron.datasets.mnist import MNIST
+from neuron.datasets.batch_iterator import BatchIterator
--- neuron/datasets/batch_iterator.py
+++ neuron/datasets/batch_iterator.py
@@ -10,12 +10,12 @@ class Batch(NamedTuple):
 
 class BatchIterator:
 
-    def __init__(self, batch_size: int, shuffle: bool = False) -> None:
+    def __init__(self, batch_size: int, shuffle: bool = True) -> None:
         self.batch_size = batch_size
         self.shuffle = shuffle
 
     def __call__(self, inputs: np.ndarray, targets: np.ndarray) -> Iterator[Batch]:
-        assert inputs.shape[-1] == targets.shape[-1], "inputs and targets must have the same length"
+        assert len(inputs) == len(targets), "inputs and targets must have the same length"
 
         starting_points = np.arange(0, len(inputs), self.batch_size)
 
@@ -25,8 +25,8 @@ class BatchIterator:
         for start in starting_points:
             end = start + self.batch_size
             
-            batch_inputs = Tensor(inputs[start:end].T)
-            batch_targets = Tensor(targets[start:end].T)
+            batch_inputs = Tensor(inputs[start:end])
+            batch_targets = Tensor(targets[start:end])
 
             yield Batch(batch_inputs, batch_targets)
 
